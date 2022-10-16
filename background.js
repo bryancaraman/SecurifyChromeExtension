4e8830226fef67b206e07c7ca59c2bb7c00dc0d3
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2022-10-16 01:24:15 -0500

    Pushing changes

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 chrome.runtime.onMessage.addListener(
     async function(request, sender, sendResponse) {
-        sendResponse("Hello World")
         if (request.fetch_scores) {
             console.log(request.emailsArray)
-            const response = await fetch("http://localhost:3000/multiple_email_info", {
+            sendResponse(() => fetch("http://localhost:3000/multiple_email_info", {
                 method: "POST",
                 headers: {
                   'Content-Type': 'application/json'
@@ -11,9 +10,8 @@ chrome.runtime.onMessage.addListener(
                 body: JSON.stringify({
                     emails: request.emailsArray,
                 }),
-            })
-            const data = await response.json();
-            sendResponse(data);
+            }))
         }
+        return true;
     }
 )
--- script.js
+++ script.js
@@ -5,12 +5,26 @@ function myMain (_) {
     for (let email of listOfEmails) {
         emailsArray.push((email.firstChild).getAttribute('email'))
     }
-    chrome.runtime.sendMessage({emailsArray, fetch_scores: true}, function (response) {
+    chrome.runtime.sendMessage({emailsArray, fetch_scores: true}, async function (response_raw) {
+        console.log(response_raw)
+        response_raw = response_raw();
+        console.log(response_raw)
+        const response = await response_raw.json();
         const listOfPictureEdges = document.getElementsByClassName("yX xY");
         const listOfRows = document.getElementsByClassName("zA")
+        console.log(response);
         for (let i = 0; i < listOfPictureEdges.length; ++i) {
             var td = document.createElement('td')
-            let src = chrome.runtime.getURL('Green_Logo.png')
+            let src = chrome.runtime.getURL('Logo_Suspicious.png')
+            if (response[i].whiteListed) {
+                src = chrome.runtime.getURL('Green_Logo.png')
+            } else if (response[i].numReports < 2) {
+                src = chrome.runtime.getURL('Green_Logo.png')
+            } else if (response[i].numReposts < 10) {
+                src = chrome.runtime.getURL('Logo_Neutral.png');
+            } else if (response[i].numReports < 50) {
+                src = chrome.runtime.getURL('Logo_Slight.png');
+            }
             var img = document.createElement("img")
             img.src = src
             img.style.marginRight = "15px";
