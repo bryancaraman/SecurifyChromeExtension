3f02e6aae13050ff292df39ea7ea4bcf46923e72
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2022-10-16 00:36:07 -0500

    Added Bryan's code

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ function myMain (_) {
         emailsArray.push((email.firstChild).getAttribute('email'))
     }
     chrome.runtime.sendMessage({emailsArray, fetch_scores: true}, function (response) {
+        const listOfPictureEdges = document.getElementsByClassName("yX xY");
+        const listOfRows = document.getElementsByClassName("zA")
+        for (let i = 0; i < listOfPictureEdges.length; ++i) {
+            var td = document.createElement('td')
+            let src = chrome.runtime.getURL('Green_Logo.png')
+            var img = document.createElement("img")
+            img.src = src
+            img.style.marginRight = "15px";
+            td.appendChild(img)
+            document.body.appendChild(td)
+            listOfRows[i].insertBefore(td, listOfPictureEdges[i])
+        }
     });
 }
