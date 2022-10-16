8815cee245f8217b04bfdf2eb4b7a2fbe2af5926
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2022-10-16 01:47:28 -0500

    Finished backend support

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
+const results = []
+
 chrome.runtime.onMessage.addListener(
     async function(request, sender, sendResponse) {
         if (request.fetch_scores) {
-            console.log(request.emailsArray)
-            sendResponse(() => fetch("http://localhost:3000/multiple_email_info", {
+            let i = results.length
+            results.push({
+                completed: false,
+            })
+            sendResponse(i)
+            const response = await fetch("http://localhost:3000/multiple_email_info", {
                 method: "POST",
                 headers: {
                   'Content-Type': 'application/json'
@@ -10,7 +16,15 @@ chrome.runtime.onMessage.addListener(
                 body: JSON.stringify({
                     emails: request.emailsArray,
                 }),
-            }))
+            })
+            const data = await response.json();
+            console.log(data)
+            results[i] = {
+                data,
+                completed: true,
+            };
+        } else if (request.get_fetch_results) {
+            sendResponse(results[request.index].data)
         }
         return true;
     }
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// 62
+
 window.addEventListener ("load", myMain, false);
 function myMain (_) {
     const listOfEmails = document.getElementsByClassName("bA4");
@@ -5,32 +7,36 @@ function myMain (_) {
     for (let email of listOfEmails) {
         emailsArray.push((email.firstChild).getAttribute('email'))
     }
-    chrome.runtime.sendMessage({emailsArray, fetch_scores: true}, async function (response_raw) {
-        console.log(response_raw)
-        response_raw = response_raw();
-        console.log(response_raw)
-        const response = await response_raw.json();
-        const listOfPictureEdges = document.getElementsByClassName("yX xY");
-        const listOfRows = document.getElementsByClassName("zA")
-        console.log(response);
-        for (let i = 0; i < listOfPictureEdges.length; ++i) {
-            var td = document.createElement('td')
-            let src = chrome.runtime.getURL('Logo_Suspicious.png')
-            if (response[i].whiteListed) {
-                src = chrome.runtime.getURL('Green_Logo.png')
-            } else if (response[i].numReports < 2) {
-                src = chrome.runtime.getURL('Green_Logo.png')
-            } else if (response[i].numReposts < 10) {
-                src = chrome.runtime.getURL('Logo_Neutral.png');
-            } else if (response[i].numReports < 50) {
-                src = chrome.runtime.getURL('Logo_Slight.png');
+    
+    let index = 0;
+    chrome.runtime.sendMessage({emailsArray, fetch_scores: true}, function (response) {
+        index = response;
+    })
+    new Promise(resolve => setTimeout(resolve, 10000)).then(() => {
+        chrome.runtime.sendMessage({index, get_fetch_results: true}, async function (response) {
+            console.log(response)
+            const listOfPictureEdges = document.getElementsByClassName("yX xY");
+            const listOfRows = document.getElementsByClassName("zA")
+            console.log(response);
+            for (let i = 0; i < listOfPictureEdges.length; ++i) {
+                var td = document.createElement('td')
+                let src = chrome.runtime.getURL('Logo_Suspicious.png')
+                if (response[i].whiteListed) {
+                    src = chrome.runtime.getURL('Green_Logo.png')
+                } else if (response[i].numReports < 2) {
+                    src = chrome.runtime.getURL('Green_Logo.png')
+                } else if (response[i].numReposts < 10) {
+                    src = chrome.runtime.getURL('Logo_Neutral.png');
+                } else if (response[i].numReports < 50) {
+                    src = chrome.runtime.getURL('Logo_Slight.png');
+                }
+                var img = document.createElement("img")
+                img.src = src
+                img.style.marginRight = "15px";
+                td.appendChild(img)
+                document.body.appendChild(td)
+                listOfRows[i].insertBefore(td, listOfPictureEdges[i])
             }
-            var img = document.createElement("img")
-            img.src = src
-            img.style.marginRight = "15px";
-            td.appendChild(img)
-            document.body.appendChild(td)
-            listOfRows[i].insertBefore(td, listOfPictureEdges[i])
-        }
-    });
+        });
+    })
 }
